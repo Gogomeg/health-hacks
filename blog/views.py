fa31f12fe0ca5d8ca2be19ae5073a815caa7625e
Gogomeg <>
2023-10-19 13:04:33 +0000

    Modify post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 
 # Render a list with the objects of the specified model
@@ -13,6 +14,23 @@ class PostList(generic.ListView):
 # How the posts are shown
 
 
-class PostDetail(generic.DetailView):
-    model = Post
+def post_detail(request, slug):
     template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
