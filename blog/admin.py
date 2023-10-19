a32f347b16e776a0cf005ccb702e563e0cf6833e
Gogomeg <>
2023-10-19 14:24:29 +0000

    Add summernote model

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ("status",)
     search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(Post, PostAdmin)
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 # Map URL for the views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
